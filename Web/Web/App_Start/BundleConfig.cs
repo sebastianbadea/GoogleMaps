c17c7cc182a12e63476e279be15d13a1eb09e2c6
sebastianbadea <sebastian.badea@gmail.com>
2015-03-20 11:57:37 +0200

    basic example

--- Web/Web/App_Start/BundleConfig.cs
+++ Web/Web/App_Start/BundleConfig.cs
@@ -26,6 +26,8 @@ namespace Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/maps").Include(
+                      "~/Scripts/google.js"));
         }
     }
 }
--- Web/Web/Scripts/google.js
+++ Web/Web/Scripts/google.js
@@ -1,1 +1,29 @@
-﻿
+﻿var maps = function () {
+    var
+        opt = null,
+        setOptions = function (options) {
+            opt = options;
+        }
+
+        initialize = function () {
+            var mapOptions = {
+                zoom: 8,
+                center: new google.maps.LatLng(-34.397, 150.644)
+            };
+            map = new google.maps.Map(opt.divMap, mapOptions);
+        }
+
+    return {
+        initialize: initialize,
+        setOptions: setOptions
+    };        
+}();
+
+$(function () {
+    var opt = {
+        divMap: document.getElementById('divMap')
+    };
+
+    maps.setOptions(opt);
+    maps.initialize();
+});
